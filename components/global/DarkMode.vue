c08e9e5168f4a1027964f30d82b345be9ab4f8e9
Ali Zemani <mehot1@gmail.com>
2024-09-21 14:31:08 +0330

    updte DEP

--- components/global/DarkMode.vue
+++ components/global/DarkMode.vue
@@ -1,23 +1,21 @@
 <script setup>
 const { isDark, toggleDark } = useDark();
+const isClient = ref(false);
+
+onMounted(() => {
+  isClient.value = true;
+});
 </script>
+
 <template>
   <div class="flex items-center">
     <UButton
-      v-if="isDark"
+      v-if="isClient"
       variant="link"
       class="text-md cursor-pointer"
       @click="toggleDark()"
     >
-      <UIcon name="i-heroicons-moon" />
-    </UButton>
-    <UButton
-      v-else
-      variant="link"
-      class="text-md cursor-pointer"
-      @click="toggleDark()"
-    >
-      <UIcon name="i-heroicons-sun" />
+      <UIcon :name="!isDark ? 'i-heroicons-sun' : 'i-heroicons-moon'" />
     </UButton>
   </div>
 </template>
